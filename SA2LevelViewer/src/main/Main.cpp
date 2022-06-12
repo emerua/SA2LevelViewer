132851748527c43c08a97cbfa8330d08c55f8917
emerua <16332209+emerua@users.noreply.github.com>
2022-06-12 15:01:20 +0900

    no comment

--- SA2LevelViewer/src/main/Main.cpp
+++ SA2LevelViewer/src/main/Main.cpp
@@ -87,7 +87,7 @@
 #include "../imgui/imgui_impl_opengl3.h"
 #include "MenuManager.h"
 
-std::string Global::version = "1.0.0_alpha";
+std::string Global::version = "1.0.0-alpha";
 
 std::unordered_set<Entity*> Global::gameEntities;
 std::list<Entity*> Global::gameEntitiesToAdd;
